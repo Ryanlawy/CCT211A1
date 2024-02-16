8d1168182bc598c639408ed201161b733cdd7ea6
Sean Kao <sean.kao@mail.utoronto.ca>
2024-02-16 18:40:15 -0500

    image

---
+++ platforms.py
@@ -0,0 +1,26 @@
+import pygame
+
+from sheet import SpriteSheet
+
+
+"""
+Platform Type:
+    1. Moving Platform 
+    2. Ground Platform
+    3. Air Platform 
+    4. Wall 
+
+"""
+
+
+
+class Platform(pygame.sprite.Sprite):
+    """Platform"""
+
+    def __init__(self, sprite_sheet_data):
+        """"""
+        pygame.sprite.Sprite.__init__(self)
+
+        # platform
+
+
