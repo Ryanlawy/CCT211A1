121f65691ed18cd98bbb2edab68393c3b360e28b
Lavey Lai <90784125+Ryanlawy@users.noreply.github.com>
2024-02-17 17:34:55 -0500

    Update globalstuff.py

--- globalstuff.py
+++ globalstuff.py
@@ -1,14 +1,20 @@
 import pygame
 
 # screen resolution
-res = (720, 720)
+#res = (720, 720)
 # opens up a window
-screen = pygame.display.set_mode(res)
+#screen = pygame.display.set_mode(res)
 
 # stores the width of the
 # screen into a variable
-SCREEN_WIDTH = screen.get_width()
+SCREEN_WIDTH = 720
 
 # stores the height of the
 # screen into a variable
-SCREEN_HEIGHT = screen.get_height()
+SCREEN_HEIGHT = 720
+
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0,   0, 255)
