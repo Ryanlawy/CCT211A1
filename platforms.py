d3281dc4b33b57328b2b1a9d7648cf92c5638223
Sean Kao <sean.kao@mail.utoronto.ca>
2024-02-17 16:10:23 -0500

    player

--- platforms.py
+++ platforms.py
@@ -1,30 +1,30 @@
-# # import pygame
+import pygame
 
-# # from sheet import SpriteSheet
-
-
-# # """
-# # Platform Type:
-# #     1. Moving Platform 
-# #     2. Ground Platform
-# #     3. Air Platform 
-# #     4. Wall 
-
-# # """
-
-# # class Platform(pygame.sprite.Sprite):
-# #     """Platform"""
-
-# #     def __init__(self, sprite_sheet_data):
-# #         """"""
-# #         pygame.sprite.Sprite.__init__(self)
-# #         # platform
-# #         self.image = 
+from sheet import SpriteSheet
 
 
+"""
+Platform Type:
+    1. Moving Platform 
+    2. Ground Platform
+    3. Air Platform 
+    4. Wall 
 
-# # class LeftToRightPlatform(Platform):
-# #     """ """
+"""
+
+class Platform(pygame.sprite.Sprite):
+    """Platform"""
+
+    def __init__(self, sprite_sheet_data):
+        """"""
+        pygame.sprite.Sprite.__init__(self)
+        # platform
+        self.image = 
+
+
+
+class LeftToRightPlatform(Platform):
+    """ """
 
 
 
